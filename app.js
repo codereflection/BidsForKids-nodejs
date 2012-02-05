9ad03c7a4d184cc2f0c16285ff3b924700e6018b
codereflection <jeff@codingreflection.com>
2012-02-05 01:40:26 -0800

    Updating port for Heroku

--- app.js
+++ app.js
@@ -39,5 +39,6 @@ app.get('/procurement/:id', procurements.view);
 app.get('/procurement/:id/view', procurements.view);
 app.get('/procurement/:id/edit', procurements.edit);
 
-app.listen(3000);
+var port = process.env.PORT || 3000;
+app.listen(port);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
