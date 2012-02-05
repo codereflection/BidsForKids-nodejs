8c38107a9511f6990e219656b9ddda455e80efaf
codereflection <jeff@codingreflection.com>
2012-02-05 01:08:34 -0800

    Basic navigation working for procurements

--- app.js
+++ app.js
@@ -23,10 +23,12 @@ app.configure(function(){
 
 app.configure('development', function(){
   app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+  app.set('view options', { pretty: true });
 });
 
 app.configure('production', function(){
   app.use(express.errorHandler()); 
+  app.set('view options', { pretty: false });
 });
 
 // Routes
@@ -34,6 +36,8 @@ app.configure('production', function(){
 app.get('/', routes.index);
 app.get('/procurements', procurements.index);
 app.get('/procurement/:id', procurements.view);
+app.get('/procurement/:id/view', procurements.view);
+app.get('/procurement/:id/edit', procurements.edit);
 
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
--- procurements.js
+++ procurements.js
@@ -7,20 +7,39 @@ var procurements = {
                 id: 100,
                 description: 'Night on the town plus hotel for 2.',
                 donation: 'Night on the town!',
-                estimatedValue: 100,
+                estimatedValue: '$800.00',
                 donor: 'Anita Schumacher',
                 procurer: 'Anita',
                 notes: 'Expires Sept 20th, 2012'
             },
+       101: {
+                id: 101,
+                description: 'A family pack of snow shoes for 4!',
+                donation: 'Snow shoes',
+                estimatedValue: '$200.00',
+                donor: 'Sheri Fantz-Gut',
+                procurer: 'Anita',
+                notes: ''
+            }
     };
 
 exports.index = function(req, res){
-  res.render('index', { title: title });    
+  res.render('procurements/index', { 
+      title: title,
+      procurements: procurements
+  });    
 };
 
 exports.view = function(req, res){
   res.render('procurements/view', {
       title: title,
-      procurement: req.procurements.id
+      procurement: procurements[req.params.id]
+  });
+};
+
+exports.edit = function(req, res){
+  res.render('procurements/edit', {
+      title: title,
+      procurement: procurements[req.params.id]
   });
 };
