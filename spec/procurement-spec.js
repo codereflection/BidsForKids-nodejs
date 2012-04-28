95d14e3759c83b40857a7b7062500df4875b42c8
codereflection <jeff@codingreflection.com>
2012-04-27 23:55:41 -0700

    Adding more simple tests

--- spec/procurement-spec.js
+++ spec/procurement-spec.js
@@ -52,17 +52,44 @@ describe('procurement-saveEdit', function(){
         req = {
             params : {
                 id : 100,
-                description: newDescription
+            },
+            body : {
+                description: newDescription,
+                donation: 'new donation',
+                estimatedValue: '$100.50',
+                donor: 'someone else',
+                procurer: 'Anita',
+                notes: 'these are my notes. there are many like them but these notes are mine.'
             }
         };
 
         procurement.saveEdit(req, res);
     });
 
-    it('should update the description of the correct procurement', function(){
+    it('should update the description', function(){
         expect(procurement.procurements[100].description).toEqual(newDescription);
     });
     
+    it('should update the donation', function(){
+        expect(procurement.procurements[100].donation).toEqual('new donation');
+    });
+
+    it('should update the estimated value', function(){
+        expect(procurement.procurements[100].estimatedValue).toEqual('$100.50');
+    });
+
+    it('should update the donor', function(){
+        expect(procurement.procurements[100].donor).toEqual('someone else');
+    });
+
+    it('should update the notes', function(){
+        expect(procurement.procurements[100].notes).toEqual('these are my notes. there are many like them but these notes are mine.');
+    });
+
+    it('should update the procurer', function(){
+        expect(procurement.procurements[100].procurer).toEqual('Anita');
+    });
+
     it('should render the procurement saved page', function() {
         expect(res.render.mostRecentCall.args[0]).toEqual('procurements/saved');
     });
